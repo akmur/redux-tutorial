dae69e0770f314d23cfe4e1d7c553ea5f8bc4d96
Alessandro Muraro <akmuraro@gmail.com>
2019-02-21 12:09:11 +0100

    fix typo

--- src/redux/stores/index.stores.js
+++ src/redux/stores/index.stores.js
@@ -6,7 +6,7 @@ import combineReducers from '../reducers/index.reducers'
 import { actionsOnDispatch } from '../middleware/index.middleware'
 // this is needed to be able to do ajax requests
 import thunk from 'redux-thunk'
-// the following line is used for the redux devtools chrome extensions
+// the following line is used for the redux devtools chrome extension
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 // creating the store
