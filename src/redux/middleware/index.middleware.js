fe43787db364deb88995919bd6fe0ddddb5bc32e
Alessandro Muraro <akmuraro@gmail.com>
2019-02-22 09:13:04 +0100

    moving ajax to middleware

--- src/redux/middleware/index.middleware.js
+++ src/redux/middleware/index.middleware.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export function actionsOnDispatch({ dispatch }) {
   return next => action => {
     if (action.type === 'USER_LOADED') {
@@ -6,6 +8,41 @@ export function actionsOnDispatch({ dispatch }) {
         payload: true
       })
     }
+
+    if (action.type === 'LOAD_NEWS') {
+      console.log('load news')
+      return axios.get('https://jsonplaceholder.typicode.com/posts').then(
+        json =>
+          new Promise(resolve => {
+            // artificial timeout simulation
+            setTimeout(() => {
+              dispatch({
+                type: 'NEWS_LOADED',
+                payload: json.data
+              })
+              resolve()
+            }, Math.floor(Math.random() * 2000) + 500)
+          })
+      )
+    }
+
+    if (action.type === 'LOAD_USER') {
+      console.log('load news')
+      return axios.get('https://jsonplaceholder.typicode.com/users/1').then(
+        json =>
+          new Promise(resolve => {
+            // artificial timeout simulation
+            setTimeout(() => {
+              dispatch({
+                type: 'USER_LOADED',
+                payload: json.data
+              })
+              resolve()
+            }, Math.floor(Math.random() * 2000) + 500)
+          })
+      )
+    }
+
     return next(action)
   }
 }
